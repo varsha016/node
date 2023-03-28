022221ea621015dcf12ad50b697cc40d67f6c387
varsha016 <103167478+varsha016@users.noreply.github.com>
2023-03-28 15:30:41 +0530

    initial

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors({
             "http://localhost:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://varsha016.github.io/versel-react"
 
 
         ]
